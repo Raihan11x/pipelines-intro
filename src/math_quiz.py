219667022a0387c95717227574332a0108b9dbd5
Raihan <69002087+Raihan11x@users.noreply.github.com>
2024-07-14 17:24:55 +0100

    modify workflows

--- src/math_quiz.py
+++ src/math_quiz.py
@@ -1,5 +1,6 @@
 import random
 import time
+import os
 
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 3
@@ -17,15 +18,22 @@ def generate_problem():
 
 def main():
     wrong = 0
-    input("Press enter to start!")
-    print("----------------------")
+    if os.getenv("NON_INTERACTIVE", "false").lower() == "true":
+        print("Running in non-interactive mode.")
+    else:
+        input("Press enter to start!")
 
+    print("----------------------")
     start_time = time.time()
 
     for i in range(TOTAL_PROBLEMS):
         expr, answer = generate_problem()
         while True:
-            guess = input(f"Problem #{i + 1}: {expr} = ")
+            if os.getenv("NON_INTERACTIVE", "false").lower() == "true":
+                guess = str(answer)  # Automatically guess the correct answer
+                print(f"Problem #{i + 1}: {expr} = {guess}")
+            else:
+                guess = input(f"Problem #{i + 1}: {expr} = ")
             if guess == str(answer):
                 break
             wrong += 1
